6c85dc36f69c352f5ae5fa40026e0b9b0f0bbfa0
hilsaj <89536200+ahils10@users.noreply.github.com>
2021-11-09 22:42:10 +0000

    Initial commit

---
+++ GuessingGame.java
@@ -0,0 +1,76 @@
+//Alexander Hils and Tanner Daria//
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GuessingGame implements ActionListener {
+  JTextField userGuess;
+  JButton pressGuess;
+  JButton pressPlayAgain;
+  JLabel enterGuess;
+  JLabel tooHighLow;
+  JLabel lastGuess;
+  Random r = new Random();
+  int randomInt;
+
+  GuessingGame() {
+    randomInt = r.nextInt(100) + 1;
+
+    JFrame f = new JFrame("Guessing Game");
+    f.setLayout(new FlowLayout());
+    f.setSize(240, 120);
+
+    userGuess = new JTextField(10);
+    userGuess.setActionCommand("myTF");
+
+    pressGuess = new JButton("Guess");
+    pressPlayAgain = new JButton("Play Again");
+
+    userGuess.addActionListener(this);
+    pressGuess.addActionListener(this);
+    pressPlayAgain.addActionListener(this);
+
+    enterGuess = new JLabel("Enter your guess:");
+    tooHighLow = new JLabel("");
+    lastGuess = new JLabel("");
+
+    f.add(enterGuess);
+    f.add(userGuess);
+    f.add(pressGuess);
+    f.add(tooHighLow);
+    f.add(lastGuess);
+    f.add(pressPlayAgain);
+    f.setVisible(true);
+  }
+
+  public void actionPerformed(ActionEvent e) {
+
+    int guess = Integer.parseInt(userGuess.getText());
+
+    if (e.getActionCommand().equals("Guess")) {
+      tooHighLow.setText("");
+      if (guess > randomInt) {
+        tooHighLow.setText("Too high!");
+      } else if (guess < randomInt) {
+        tooHighLow.setText("Too low!");
+      } else {
+        tooHighLow.setText("You got it!");
+      }
+      lastGuess.setText("Last guess was " + userGuess.getText());
+    }
+
+    else if (e.getActionCommand().equals("Play Again")) {
+      Random r = new Random();
+      randomInt = r.nextInt(100) + 1;
+      enterGuess.setText("Enter your guess:");
+      tooHighLow.setText("");
+      lastGuess.setText("");
+      userGuess.setText("");
+    }
+  
+    else {
+     tooHighLow.setText("You pressed Enter. Please press the Guess Button.");
+    }
+ }
+}
